ed51809c2ac8c298484297a78408b8ad99bd37f2
yuminhwan <ymh7532@icloud.com>
2023-07-10 23:58:21 +0900

    feat: 랭킹 상금금액 추가

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 import java.util.function.BiPredicate;
 
 enum Ranking {
-    ONE((sameNumberCount, hasBonusNumber) -> sameNumberCount == 6),
-    TWO((sameNumberCount, hasBonusNumber) -> sameNumberCount == 5 && hasBonusNumber),
-    THREE((sameNumberCount, hasBonusNumber) -> sameNumberCount == 5 && !hasBonusNumber),
-    FOUR((sameNumberCount, hasBonusNumber) -> sameNumberCount == 4),
-    FIVE((sameNumberCount, hasBonusNumber) -> sameNumberCount == 3),
-    NONE((sameNumberCount, hasBonusNumber) -> sameNumberCount < 3);
+    ONE(2_000_000_000, (sameNumberCount, hasBonusNumber) -> sameNumberCount == 6),
+    TWO(30_000_000, (sameNumberCount, hasBonusNumber) -> sameNumberCount == 5 && hasBonusNumber),
+    THREE(1_500_000, (sameNumberCount, hasBonusNumber) -> sameNumberCount == 5 && !hasBonusNumber),
+    FOUR(50_000, (sameNumberCount, hasBonusNumber) -> sameNumberCount == 4),
+    FIVE(5_000, (sameNumberCount, hasBonusNumber) -> sameNumberCount == 3),
+    NONE(0, (sameNumberCount, hasBonusNumber) -> sameNumberCount < 3);
 
+    private final int prize;
     private final BiPredicate<Integer, Boolean> checkRanking;
 
-    Ranking(BiPredicate<Integer, Boolean> rankPredicate) {
+    Ranking(int prize, BiPredicate<Integer, Boolean> rankPredicate) {
+        this.prize = prize;
         this.checkRanking = rankPredicate;
     }
 
