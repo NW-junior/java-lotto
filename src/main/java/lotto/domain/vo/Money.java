387ce1ae0f1a42a0e6e597ce0a16f419a37b7d0a
yuminhwan <ymh7532@icloud.com>
2023-07-12 01:39:55 +0900

    refactor: 자료형 변경

--- src/main/java/lotto/domain/vo/Money.java
+++ src/main/java/lotto/domain/vo/Money.java
@@ -1,17 +1,17 @@
 package lotto.domain.vo;
 
 public class Money {
-    private static final int MIN_MONEY_VALUE = 1_000;
-    private static final int VALID_SUBTRACT_VALUE = 0;
+    private static final long MIN_MONEY_VALUE = 1_000L;
+    private static final long VALID_SUBTRACT_VALUE = 0L;
 
-    private final int value;
+    private final long value;
 
-    public Money(int value) {
+    public Money(long value) {
         validateValue(value);
         this.value = value;
     }
 
-    private void validateValue(int value) {
+    private void validateValue(long value) {
         if (value < MIN_MONEY_VALUE) {
             throw new IllegalArgumentException(
                 String.format("구입 금액은 %d원 이상 이여야 합니다. 입력 금액 : %d", MIN_MONEY_VALUE, value));
@@ -24,7 +24,7 @@ public class Money {
     }
 
     public int calculateLottoCount() {
-        return this.value / MIN_MONEY_VALUE;
+        return (int)(this.value / MIN_MONEY_VALUE);
     }
 
 }
