81e697383b52a1183180a75a2f3755b3ab895e39
yuminhwan <ymh7532@icloud.com>
2023-07-17 23:14:39 +0900

    refactor: 전체적인 흐름 조절하는 객체 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,70 +1,10 @@
 package lotto;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import lotto.domain.Lotto;
-import lotto.domain.LottoTicket;
-import lotto.domain.Ranking;
-import lotto.domain.WinningLotto;
-import lotto.domain.WinningResult;
-import lotto.domain.vo.LottoNumber;
-import lotto.domain.vo.Money;
-import lotto.view.InputView;
-import lotto.view.OutputView;
-
 public class Application {
+
     public static void main(String[] args) {
-        Money money = inputMoney();
-        LottoTicket lottoTicket = LottoTicket.publishLottoTicket(money.calculateLottoCount());
-
-        OutputView.printBuyLottos(lottoTicket.getValue());
-
-        Lotto lotto = inputLotto();
-
-        LottoNumber bounsNumber = inputBounsNumber();
-        WinningLotto winningLotto = new WinningLotto(lotto, bounsNumber);
-
-        List<Ranking> rankings = lottoTicket.calculateRankings(winningLotto);
-        WinningResult winningResult = new WinningResult(rankings);
-        OutputView.printResult(winningResult, money);
-    }
-
-    private static Money inputMoney() {
-        while (true) {
-            try {
-                return new Money(InputView.inputMoney());
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e.getMessage());
-            }
-        }
-    }
-
-    private static Lotto inputLotto() {
-        while (true) {
-            try {
-                List<LottoNumber> inputLottoNumbers = Stream.of(InputView.inputWinningLottoNumbers().split(","))
-                    .map(String::trim)
-                    .map(Integer::parseInt)
-                    .map(LottoNumber::new)
-                    .collect(Collectors.toList());
-
-                return new Lotto(inputLottoNumbers);
-            } catch (Exception e) {
-                OutputView.printError(e.getMessage());
-            }
-        }
-    }
-
-    private static LottoNumber inputBounsNumber() {
-        while (true) {
-            try {
-                return new LottoNumber(InputView.inputBonusNumber());
-            } catch (IllegalArgumentException e) {
-                OutputView.printError(e.getMessage());
-            }
-        }
+        LottoMachine lottoMachine = new LottoMachine();
+        lottoMachine.run();
     }
 
 }
